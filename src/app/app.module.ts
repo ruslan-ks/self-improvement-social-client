2725347655ce1f5c779406a202cdb43f2ed836e3
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-04-21 15:49:57 +0300

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { LanguageSelectComponent } from './component/language-select/language-select.component';
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { HeaderComponent } from './component/header/header.component';
 import { LoginFormComponent } from './component/login-form/login-form.component';
-import { FormsModule } from "@angular/forms";
+import { HomeComponent } from './component/home/home.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginFormComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     LanguageSelectComponent,
     HeaderComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     HttpClientModule,
     TranslateModule.forRoot({
@@ -33,7 +43,7 @@ export function HttpLoaderFactory(http: HttpClient) {
         deps: [HttpClient]
       }
     }),
-    FormsModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
