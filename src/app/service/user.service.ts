67098b306d6dad659669f29e081683d2f6dd21c7
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-02 23:30:58 +0300

    Add getCount method to UserService

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { ResponseBody } from "../interface/response-body";
-import { map, Observable, throwError } from "rxjs";
-import { catchError, shareReplay } from "rxjs/operators";
+import { Observable, throwError } from "rxjs";
+import { catchError, shareReplay, tap, map } from "rxjs/operators";
 import { ShortUserData } from "../interface/short-user-data";
 import { PageRequest } from "./page-request";
 
@@ -30,6 +30,24 @@ export class UserService {
   private extractUsersArray(response: ResponseBody): ShortUserData[] {
     console.log('Obtained users:', (<any> response.data).users);
     return (<any> response.data).users;
+  }
+
+  getCount(): Observable<number> {
+    return this.http.get<ResponseBody>(this.apiUrl + '/count')
+      .pipe(
+        map(this.extractCount),
+        tap(this.logCount),
+        catchError(this.handleError),
+        shareReplay()
+      );
+  }
+
+  private extractCount(response: ResponseBody): number {
+    return (<any> response.data).count;
+  }
+
+  private logCount(count: number): void {
+    console.log('Obtained users count:', count);
   }
 
   // save$ = (user: User) => <Observable<ResponseBody>>this.http.post(this.apiUrl, user)
