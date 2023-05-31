d89309fbcb8178cb3da537d9fa44ef5e48d01b39
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-05-31 13:17:41 +0300

    Add ActivityService; add GetParamsBuilder

---
+++ src/app/dto/request/fitler/get-params-builder.service.ts
@@ -0,0 +1,39 @@
+import { FilterCriteria } from "./filter-criteria";
+import { FilterOperation } from "./filter-operation";
+import { NoSuchElementError } from "../../../error/no-such-element-error";
+import { Injectable } from "@angular/core";
+import { EntityPageRequest } from "../page/entity-page-request";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetParamsBuilder {
+  private operationCodeMap: Map<FilterOperation, string> = new Map<FilterOperation, string>([
+    [FilterOperation.EQUAL, 'eq'],
+    [FilterOperation.LIKE, 'lk'],
+    [FilterOperation.CONTAINS, 'cn'],
+    [FilterOperation.GREATER_EQUAL, 'ge'],
+    [FilterOperation.LESS_EQUAL, 'le']
+  ]);
+
+  build(filters: FilterCriteria[], pageRequest: EntityPageRequest | undefined): string {
+    let pageParams = '';
+    if (typeof pageRequest !== undefined) {
+      pageParams = Object.entries(pageRequest!)
+        .map((k, v) => k + '=' + v)
+        .join('&');
+      pageParams += '&';
+    }
+    const filterQuery = filters
+      .map(f => f.field + ' ' + this.toCode(f.operation) + ' ' + f.value)
+      .join(';');
+    return '?' + pageParams + filterQuery;
+  }
+
+  private toCode(operation: FilterOperation): string {
+    if (!this.operationCodeMap.has(operation)) {
+      throw new NoSuchElementError("Key '" + operation + "' not found");
+    }
+    return this.operationCodeMap.get(operation)!;
+  }
+}
---
+++ src/app/service/activity.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { FilterCriteria } from "../dto/request/fitler/filter-criteria";
+import { HttpClient } from "@angular/common/http";
+import { GetParamsBuilder } from "../dto/request/fitler/get-params-builder.service";
+import { EntityPageRequest } from "../dto/request/page/entity-page-request";
+import { AppSettings } from "../app-settings";
+import { map, shareReplay } from "rxjs/operators";
+import { ResponseBody } from "../dto/response/response-body";
+import { ActivityPageResponse } from "../dto/response/page/activity-page-response";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityService {
+  constructor(private http: HttpClient, private getParamsBuilder: GetParamsBuilder) {}
+
+  page$ = (pageRequest: EntityPageRequest, filters: FilterCriteria[]): Observable<ActivityPageResponse> => {
+    const urlParams = this.getParamsBuilder.build(filters, pageRequest);
+    return this.http.get<ResponseBody>(AppSettings.API_URL + 'activities' + urlParams)
+      .pipe(
+        map(this.extractActivityPageResponse),
+        shareReplay()
+      );
+  }
+
+  private extractActivityPageResponse(responseBody: ResponseBody): ActivityPageResponse {
+    const data = <any> responseBody.data;
+    return {
+      page: data.page,
+      size: data.size,
+      count: data.count,
+      activities: data.activities
+    };
+  }
+}
