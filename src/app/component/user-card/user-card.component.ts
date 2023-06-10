f054d8eeb75818fb79f4f1e5eceb2ac327268937
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-06-10 23:16:51 +0300

    Complete user-card component; replace user profile with user-card

--- src/app/component/user-card/user-card.component.ts
+++ src/app/component/user-card/user-card.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { User } from "../../interface/user";
+import { Observable } from "rxjs";
+import { UserActivity } from "../../interface/user-activity";
+import { ActivatedRoute } from "@angular/router";
+import { UserService } from "../../service/user.service";
+import { UserActivityService } from "../../service/user-activity.service";
+import { ActivityService } from "../../service/activity.service";
+import { PageRequest } from "../../dto/request/page/page-request";
+import { FilterCriteria } from "../../dto/request/fitler/filter-criteria";
+import { FilterOperator } from "../../dto/request/fitler/filter-operator";
+import { EntityPageRequest } from "../../dto/request/page/entity-page-request";
+import { map } from "rxjs/operators";
+import { AuthService } from "../../service/auth.service";
 
 @Component({
   selector: 'app-user-card',
@@ -9,8 +21,33 @@ import { User } from "../../interface/user";
 export class UserCardComponent {
   @Input({ required: true }) user: User | null = null;
 
-  toAvatarUrl(user: User | null): string {
-    return `http://localhost:8080/users/${user?.id}/avatar`;
+  userActivities$: Observable<UserActivity[]>;
+  userActivityCount$: Observable<number>;
+  createdActivitiesCount$: Observable<number>;
+  followersCount$: Observable<number>;
+  followingsCount$: Observable<number>;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private userService: UserService,
+              private userActivityService: UserActivityService,
+              private activityService: ActivityService,
+              private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userActivities$ = this.userActivityService.getPage$(this.user.id, PageRequest.getDefault());
+    this.userActivityCount$ = this.userActivityService.count$(this.user.id);
+
+    const filterCriteria = new FilterCriteria("author", FilterOperator.EQUAL, this.user.id);
+    this.createdActivitiesCount$ = this.activityService.page$(EntityPageRequest.getDefault(), [filterCriteria])
+      .pipe(
+        map(dataMap => dataMap.count)
+      );
+
+    this.followersCount$ = this.userService.followersCount(this.user.id);
+    this.followingsCount$ = this.userService.followingsCount(this.user.id);
   }
 
+  getAvatarUrl(user: User | null): string {
+    return `http://localhost:8080/users/${user?.id}/avatar`;
+  }
 }
