d3f95f50e9f75c27fffe04cad59bbc187379ceab
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-06-11 14:47:45 +0300

    Add user activities size check before displaying user activities statistics

--- src/app/component/user-profile/user-profile.component.ts
+++ src/app/component/user-profile/user-profile.component.ts
@@ -6,6 +6,7 @@ import { UserService } from "../../service/user.service";
 import { UserActivity } from "../../interface/user-activity";
 import { UserActivityService } from "../../service/user-activity.service";
 import { PageRequest } from "../../dto/request/page/page-request";
+import { AuthService } from "../../service/auth.service";
 
 @Component({
   selector: 'app-user-profile',
@@ -18,11 +19,17 @@ export class UserProfileComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute,
               private userService: UserService,
-              private userActivityService: UserActivityService) {}
+              private userActivityService: UserActivityService,
+              private authService: AuthService) {}
 
   ngOnInit(): void {
     const userId: number = +this.activatedRoute.snapshot.paramMap.get('id')!;
     this.user$ = this.userService.getById$(userId);
     this.userActivities$ = this.userActivityService.getPage$(userId, PageRequest.getDefault());
+  }
+
+  isNotLoggedUserId(userId: number) {
+    const loggedUserId = this.authService.getLoggedUserId();
+    return !(loggedUserId && loggedUserId === userId);
   }
 }
